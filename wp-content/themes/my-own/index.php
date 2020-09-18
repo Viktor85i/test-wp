2d7657e606d788e0d9833e31a2dfbd4643672d73
Viktor Brazhnyk <viktor.brazhnyk@dreamdev.email>
2020-09-18 17:50:29 +0300

    1

---
+++ wp-content/themes/my-own/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: viktorbrazhnyk
+ * Date: 18.09.20
+ * Time: 17:31
+ */
+get_header(); ?>
+
+index
+
+<?php get_footer(); ?>
---
+++ wp-content/themes/my/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: viktorbrazhnyk
+ * Date: 18.09.20
+ * Time: 17:34
+ */
+add_action('wp_head', function () {
+    echo '<meta name="hren">';
+});
